fe12355c8c98cf72beb9588bbf8388da8e2b67f4
Gabriel Wanjau <gabrielwanaju832@gmail.com>
2026-02-16 09:20:30 +0000

    initial commit

---
+++ avgTemp.py
@@ -0,0 +1,26 @@
+import random
+def get_random_weather_data():
+    weather_data = {
+        'temp' :round(random.uniform(-50, 50)),
+        'humidity' : random.randint(0, 100),
+        'pressure': random.randint(990, 1011)
+    }
+    
+    weather_data['feels_like'] = round(random.uniform((weather_data['temp'] - 10), (weather_data['temp'] + 10)), 2)
+    
+    return weather_data
+    
+    
+list = []
+for i in range(1, 101):
+    list.append(get_random_weather_data())
+
+def get_average_temperature(list) :
+    sum = 0
+    for i in list:
+        sum += i['temp']
+        
+    return sum/100
+    
+print(get_average_temperature(list))
+
